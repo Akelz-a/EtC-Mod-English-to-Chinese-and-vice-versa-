013f94ba2bcbabc9a7c4b96170013111df2d7d89
Akelz-a <aliyarimel@gmail.com>
2026-09-23 01:38:11 +0900

    template

---
+++ src/client/java/akelza/etctranslate/mod/client/EtCEnglishToChineseAndViceVersaClient.java
@@ -0,0 +1,11 @@
+package akelza.etctranslate.mod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EtCEnglishToChineseAndViceVersaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		//test
+	}
+}
